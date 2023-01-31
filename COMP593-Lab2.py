c54768aef2796a6276afd0541898f053436ecb1f
JayRWheeler <Jay.Wheeler@flemingcollege.ca>
2023-01-30 22:34:59 -0500

    Redid Code and Cleaned It
    - Redid the whole script so it would flow nicer
    
    - Got everything to work I think

--- COMP593-Lab2.py
+++ COMP593-Lab2.py
@@ -2,28 +2,43 @@ def main():
 
     # TODO: Step 2 - Create a complex data structure
     about_me = {
-    'Full Name': 'James Wheeler',
-    'Student_ID': 10279432,
-    'Pizza_Toppings': [
-        'Pepperoni', 
-        'Sausage', 
-        'Olives'
-    ],
-    'Movies': [
-        'The Menu',
-        'Knives Out: Glass Onion',
-        'Last Night In Soho'
-    ]
-}
+        'Full Name': 'James Wheeler',
+        'Student_ID': 10279432,
+        'Pizza_Toppings': [
+            'Pepperoni', 
+            'Sausage', 
+            'Olives'
+        ],
+        'Movies': [
+            {
+                'Title': 'The Menu',
+                'Genre': 'Comedy'
+            },
+            {
+                'Title': 'Knives Out: Glass Onion',
+                'Genre': 'Mystery'
+            },
+            {
+                'Title': 'Last Night In Soho',
+                'Genre': 'Thriller'
+            }
+        ]
+    }
 
     # TODO: Step 3 - Add another movie to the data structure
     about_me['Movies'].insert(1, 'Batman: Assault on Arkham')
-    print (about_me)
+   
+    print_student_name_and_id(about_me)
+    add_pizza_toppings(about_me, ['Bacon', 'Hot Peppers'])
+    print_pizza_toppings(about_me)
+    print_movie_genres(about_me)
+    print_movie_titles(about_me)
+
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
     Full_Name = about_me['Full Name'].split()
     First_Name = Full_Name[0]
-    print(f"My name is {about_me['Full_Name'].title()} but you can call me High Supreme Omega Galactic Emperor{about_me[First_Name].title}")
+    print(f"My name is {about_me['Full Name'].title()} but you can call me High Supreme Omega Galactic Emperor {First_Name.title()}")
     print(f"My student ID is {about_me['Student_ID']}")
     return
     
@@ -35,26 +50,29 @@ def add_pizza_toppings(about_me, toppings):
         about_me['Pizza_Toppings'][i] = p.lower()
 
     about_me['Pizza_Toppings'].sort()
-    
     return
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
 def print_pizza_toppings(about_me):
     print('\nMy favorite pizza toppings are:')
     for topping in about_me['Pizza_Toppings']:
-        print(f"\t- {topping.title()}")
+        print(f"\t- {topping.upper()}")
     return
 
 # TODO: Step 7 - Function that prints comma-separated list of movie genres
 def print_movie_genres(about_me):
-    
-    return 
+    print("\nI like to watch", end = " ")
+    genres = [movie['Genre'] for movie in about_me['Movies']]
+    genres = list(set(genres))
+    print(", ".join(genres), end = " ")
+    print("movies.")
+    return
 
 # TODO: Step 8 - Function that prints comma-separated list of movie titles
-def print_movie_titles(movie_list):
-    print("My favorite movies are:")
-    print(', '.join(movie_list))
+def print_movie_titles(about_me):
+    print("Some of my favorite movies are", end = " ")
+    titles = [movie['Title'] for movie in about_me['Movies']]
+    print(", ".join(titles) + ".")
     return
-    
 if __name__ == '__main__':
     main()
